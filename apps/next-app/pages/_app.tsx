75e57e7b320bd7f2ef39f1a98d479af0f58d8945
Mat Warger <mwarger@gmail.com>
2022-07-28 15:20:33 -0500

    removed styles

--- apps/next-app/pages/_app.tsx
+++ apps/next-app/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import Head from 'next/head'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
